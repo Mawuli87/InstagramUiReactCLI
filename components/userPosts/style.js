81e9791a14659bd5d82138cfadeb548d8d78cf32
Mawuli87 <yawomessie@gmail.com>
2023-12-23 23:49:08 +0000

    Using scaling function de ajust to different screen size

--- components/userPosts/style.js
+++ components/userPosts/style.js
@@ -1,17 +1,23 @@
 import {StyleSheet} from 'react-native';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../style/scaling';
 
 const style = StyleSheet.create({
   name: {
     fontFamily: 'Inter',
     fontWeight: '500',
-    fontSize: 16,
-    lineHeight: 19,
+    fontSize: scaleFontSize(16),
+    lineHeight: scaleFontSize(19),
   },
   location: {
     fontFamily: 'Inter',
     fontWeight: '400',
-    fontSize: 12,
-    lineHeight: 15,
+    fontSize: scaleFontSize(12),
+    lineHeight: scaleFontSize(15),
+    paddingTop: verticalScale(5),
     color: '#79869F',
   },
   userInformation: {
@@ -20,13 +26,7 @@ const style = StyleSheet.create({
     alignItems: 'center',
   },
   userPostInformation: {
-    marginLeft: 10,
-  },
-  image: {
-    borderWidth: 1,
-    borderColor: '#F35BAC',
-    borderRadius: 50,
-    padding: 3,
+    marginLeft: horizontalScale(10),
   },
   userInformationContainer: {
     flex: 1,
@@ -37,7 +37,6 @@ const style = StyleSheet.create({
   userPostContainer: {
     borderBottomWidth: 1,
     borderColor: '#EFF2F6',
-    marginBottom: 20,
   },
   userPostStatText: {
     color: '#79869F',
@@ -46,21 +45,21 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     flexDirection: 'row',
-    marginVertical: 16,
+    marginVertical: verticalScale(16),
   },
   userPostStats: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
-    paddingHorizontal: 10,
+    paddingBottom: verticalScale(30),
+    paddingHorizontal: horizontalScale(10),
   },
   userPostStatButton: {
-    marginRight: 27,
+    marginRight: horizontalScale(27),
     flexDirection: 'row',
   },
   userPostStatIcon: {
-    marginRight: 3,
+    marginRight: horizontalScale(3),
     color: '#79869F',
   },
 });
--- components/userStory/style.js
+++ components/userStory/style.js
@@ -1,22 +1,21 @@
 import {StyleSheet} from 'react-native';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../style/scaling';
 
 const style = StyleSheet.create({
   storyContainer: {
-    marginRight: 20,
+    marginRight: horizontalScale(18),
   },
   name: {
     textAlign: 'center',
     fontFamily: 'Inter',
     fontWeight: '500',
-    fontSize: 14,
-    lineHeight: 17,
-    marginTop: 8,
-  },
-  userImageContainer: {
-    borderWidth: 1,
-    borderColor: '#F35BAC',
-    padding: 3,
-    borderRadius: 50,
+    fontSize: scaleFontSize(14),
+    lineHeight: scaleFontSize(17),
+    marginTop: verticalScale(8),
   },
 });
 
